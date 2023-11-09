b3f756872ee6df31ceb7929ec14c3a22008a4918
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2023-11-09 17:22:17 -0500

    tweak

--- pure_phase.py
+++ pure_phase.py
@@ -7,25 +7,26 @@ class XRDPattern():
         self.wl = wl
         self.q_max = 4*np.pi/self.wl
 
-    def gen_compatible_qs(self, tol = 1E-3):
-        for i_c in range(len(self.qs)):
-            for i_b in range(0, i_c + 1):
-                for i_a in range(0, i_b + 1):
-                    q_a = self.qs[i_a]
-                    q_b = self.qs[i_b]
-                    q_c = self.qs[i_c]
-                    for q_d in self.qs[((q_c-q_b+tol) < self.qs) * (self.qs < ((q_c**2+q_b**2)**0.5+tol))]:
-                        for q_e in self.qs[((q_c-q_a+tol) < self.qs) * (self.qs < ((q_c**2+q_a**2)**0.5+tol))]:
-                            x_1 = (q_a**2+q_c**2-q_e**2)/(2*q_c)
-                            x_2 = (q_b**2+q_c**2-q_d**2)/(2*q_c)
-                            y_1 = (abs(q_a**2-x_1**2))**0.5
-                            y_2 = (abs(q_b**2-x_2**2))**0.5
-                            lower = (abs(q_a**2+q_b**2-2*x_1*x_2-2*y_1*y_2))**0.5
-                            upper = (abs(q_a**2+q_b**2-2*x_1*x_2))**0.5
-                            for q_f in self.qs[(lower + tol < self.qs) * (self.qs < upper + tol)]:
-                                yield q_a, q_b, q_c, q_d, q_e, q_f
+    def gen_compatible_qs(self, tol = 1E-5):
+        q_a = self.qs[0]
+        q_b = self.qs[np.nonzero((self.qs % self.qs[0]) > tol)[0][0]]
 
-    def get_rec_lattice(self, tol = 1E-3):
+        for i_c in range(len(self.qs)):
+            q_c = self.qs[i_c]
+            for q_d in self.qs[((q_c-q_b+tol) < self.qs) * (self.qs < ((q_c**2+q_b**2)**0.5+tol))]:
+                for q_e in self.qs[((q_c-q_a+tol) < self.qs) * (self.qs < ((q_c**2+q_a**2)**0.5+tol))]:
+                    x_1 = (q_a**2+q_c**2-q_e**2)/(2*q_c)
+                    x_2 = (q_b**2+q_c**2-q_d**2)/(2*q_c)
+                    y_1 = (abs(q_a**2-x_1**2))**0.5
+                    y_2 = (abs(q_b**2-x_2**2))**0.5
+                    lower = (abs(q_a**2+q_b**2-2*x_1*x_2-2*y_1*y_2))**0.5
+                    upper = (abs(q_a**2+q_b**2-2*x_1*x_2))**0.5
+                    for q_f in self.qs[(lower + tol < self.qs) * (self.qs < upper + tol)]:
+                        yield q_a, q_b, q_c, q_d, q_e, q_f
+
+    def get_rec_lattice(self, tol = 1E-5):
+        min_opt_dif = 100
+        best_params = None
         for q_a, q_b, q_c, q_d, q_e, q_f in self.gen_compatible_qs():
             cq_alpha = max(min((q_b**2 + q_c**2 - q_d**2)/(2*q_b*q_c), 1), -1)
             cq_beta = max(min((q_c**2 + q_a**2 - q_e**2)/(2*q_c*q_a), 1), -1)
@@ -42,13 +43,9 @@ class XRDPattern():
             q_dif_all = np.abs(self.qs.reshape((-1, 1))-q_tmp)
             q_dif_idx = np.argmin(q_dif_all, axis = 1).flatten()
             q_dif = np.array([q_dif_all[i, min_idx_i] for i, min_idx_i in enumerate(q_dif_idx)])
+            if np.max(q_dif) < min_opt_dif:
+                min_opt_dif = np.max(q_dif)
+                best_params = q_a, q_b, q_c, q_alpha, q_beta, q_gamma
             if np.all(q_dif < tol):
-                ns = points[0][q_dif_idx]
-                converter = np.zeros((6, 3, 3))
-                converter[0, 0, 0] = 1
-                converter[1, 1, 1] = 1
-                converter[2, 2, 2] = 1
-                converter[3, 1, 2] = 2
-                converter[4, 2, 0] = 2
-                converter[5, 0, 2] = 2
-                X = np.einsum('ijk,lj,mk->ilm', converter, q_dif_idx, q_dif_idx)
+                return best_params
+        return best_params
--- test_mp_pure.py
+++ test_mp_pure.py
@@ -8,28 +8,31 @@ p = 0
 n = 0
 times = []
 for r in range(1, R + 1):
-    print(f'round {r}, {n} invalid rounds, pass rate is {round(100*p/(r-n), 2)} %', end = '\r', flush = True)
+    print(f'round {r}, {n} invalid rounds, pass rate is {round(100*p/(r-n), 2)} %')
+    start = time.time()
+    a = np.random.uniform(2, 5)
+    b = np.random.uniform(2, 5)
+    c = np.random.uniform(2, 5)
+    alpha = np.random.uniform(30, 100)
+    beta = np.random.uniform(30, 100)
+    gamma = np.random.uniform(30, 100)
     try:
-        start = time.time()
-        a = np.random.uniform(2, 5)
-        b = np.random.uniform(2, 5)
-        c = np.random.uniform(2, 5)
-        alpha = np.random.uniform(30, 100)
-        beta = np.random.uniform(30, 100)
-        gamma = np.random.uniform(30, 100)
-
         lattice = Lattice.from_parameters(a, b, c, alpha, beta, gamma)
         rec_lattice = lattice.reciprocal_lattice
         qs = np.sort(rec_lattice.get_points_in_sphere([[0, 0, 0]], [0, 0, 0], 4*np.pi, zip_results = False)[1])[1::2]
-        xrdp = XRDPattern(qs[:10])
-        pred_rec_lattice = Lattice.from_parameters(*xrdp.get_rec_lattice())
-        pred_lattice = pred_rec_lattice.reciprocal_lattice
-        if lattice.find_mapping(pred_lattice):
-            p += 1
-        stop = time.time()
-        times.append(stop - start)
     except:
         n += 1
+        continue
+    if len(qs) < 6:
+        n += 1
+        continue
+    xrdp = XRDPattern(qs[:10])
+    pred_rec_lattice = Lattice.from_parameters(*xrdp.get_rec_lattice())
+    pred_lattice = pred_rec_lattice.reciprocal_lattice
+    if lattice.find_mapping(pred_lattice):
+        p += 1
+    stop = time.time()
+    times.append(stop - start)
 
 print(f'After {R} trials, {n} invalid rounds, pass rate is {round(100*p/(R-n), 2)} %')
 print(f'Average time: {np.mean(times)} +- {np.std(times)}')
